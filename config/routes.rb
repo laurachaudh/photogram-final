61c37f55be933b6b3ab60c72a51a36050a592902
laurachaudh <laurachaudhary@chicagobooth.edu>
2024-12-06 19:58:30 +0000

    models and tables added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,37 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Devise routes for users
+  devise_for :users
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # Root route
+  root to: "photos#index"
+
+  # Routes for the Follow request resource
+  post("/insert_follow_request", { :controller => "follow_requests", :action => "create" })
+  get("/follow_requests", { :controller => "follow_requests", :action => "index" })
+  get("/follow_requests/:path_id", { :controller => "follow_requests", :action => "show" })
+  post("/modify_follow_request/:path_id", { :controller => "follow_requests", :action => "update" })
+  get("/delete_follow_request/:path_id", { :controller => "follow_requests", :action => "destroy" })
+
+  # Routes for the Like resource
+  post("/insert_like", { :controller => "likes", :action => "create" })
+  get("/likes", { :controller => "likes", :action => "index" })
+  get("/likes/:path_id", { :controller => "likes", :action => "show" })
+  post("/modify_like/:path_id", { :controller => "likes", :action => "update" })
+  get("/delete_like/:path_id", { :controller => "likes", :action => "destroy" })
+
+  # Routes for the Comment resource
+  post("/insert_comment", { :controller => "comments", :action => "create" })
+  get("/comments", { :controller => "comments", :action => "index" })
+  get("/comments/:path_id", { :controller => "comments", :action => "show" })
+  post("/modify_comment/:path_id", { :controller => "comments", :action => "update" })
+  get("/delete_comment/:path_id", { :controller => "comments", :action => "destroy" })
+
+  # Routes for the Photo resource
+  post("/insert_photo", { :controller => "photos", :action => "create" })
+  get("/photos", { :controller => "photos", :action => "index" })
+  get("/photos/:path_id", { :controller => "photos", :action => "show" })
+  post("/modify_photo/:path_id", { :controller => "photos", :action => "update" })
+  get("/delete_photo/:path_id", { :controller => "photos", :action => "destroy" })
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,57 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2024_12_06_192035) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body"
+    t.integer "photo_id"
+    t.integer "author_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "follow_requests", force: :cascade do |t|
+    t.integer "sender_id"
+    t.integer "recipient_id"
+    t.string "status"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "photo_id"
+    t.integer "fan_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.text "caption"
+    t.string "image"
+    t.integer "likes_count"
+    t.integer "comments_count"
+    t.integer "owner_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.boolean "private"
+    t.integer "likes_count"
+    t.integer "comments_count"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
 end
