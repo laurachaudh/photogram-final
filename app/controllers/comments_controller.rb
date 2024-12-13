6a676901bf53cf7dbd4ec6b3f48186568bef5294
laurachaudh <laurachaudhary@chicagobooth.edu>
2024-12-13 02:20:24 +0000

    submitting comments works!

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,58 +1,23 @@
 class CommentsController < ApplicationController
-  def index
-    matching_comments = Comment.all
-
-    @list_of_comments = matching_comments.order({ :created_at => :desc })
-
-    render({ :template => "comments/index" })
-  end
-
-  def show
-    the_id = params.fetch("path_id")
-
-    matching_comments = Comment.where({ :id => the_id })
-
-    @the_comment = matching_comments.at(0)
-
-    render({ :template => "comments/show" })
-  end
+  before_action :authenticate_user!
 
   def create
-    the_comment = Comment.new
-    the_comment.body = params.fetch("query_body")
-    the_comment.photo_id = params.fetch("query_photo_id")
-    the_comment.author_id = params.fetch("query_author_id")
+    # Ensure the photo exists
+    @photo = Photo.find(params[:photo_id])
+    comment = @photo.comments.new(comment_params)
+    comment.author_id = current_user.id
 
-    if the_comment.valid?
-      the_comment.save
-      redirect_to("/comments", { :notice => "Comment created successfully." })
+    if comment.save
+      @photo.increment!(:comments_count)
+      redirect_to photo_path(@photo), notice: "Comment added successfully."
     else
-      redirect_to("/comments", { :alert => the_comment.errors.full_messages.to_sentence })
+      redirect_to photo_path(@photo), alert: "Unable to add comment: #{comment.errors.full_messages.to_sentence}."
     end
   end
 
-  def update
-    the_id = params.fetch("path_id")
-    the_comment = Comment.where({ :id => the_id }).at(0)
+  private
 
-    the_comment.body = params.fetch("query_body")
-    the_comment.photo_id = params.fetch("query_photo_id")
-    the_comment.author_id = params.fetch("query_author_id")
-
-    if the_comment.valid?
-      the_comment.save
-      redirect_to("/comments/#{the_comment.id}", { :notice => "Comment updated successfully."} )
-    else
-      redirect_to("/comments/#{the_comment.id}", { :alert => the_comment.errors.full_messages.to_sentence })
-    end
-  end
-
-  def destroy
-    the_id = params.fetch("path_id")
-    the_comment = Comment.where({ :id => the_id }).at(0)
-
-    the_comment.destroy
-
-    redirect_to("/comments", { :notice => "Comment deleted successfully."} )
+  def comment_params
+    params.require(:comment).permit(:body, :photo_id)
   end
 end
